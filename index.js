3775559362022397d02c1ff520f60b9468c16225
Hassan Ahmad <hassanahmad.6h1@gmail.com>
2023-11-25 13:32:40 +0500

    update

---
+++ index.js
@@ -0,0 +1,9 @@
+module.exports = {
+    greet: function (name) {
+      return `Hello, ${name}! Welcome to my test package.`;
+    },
+    add: function (a, b) {
+      return a + b;
+    },
+  };
+  
